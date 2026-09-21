342230d40a64d1c83f0bc707f23c9fa9554791a0
Rarnak <rarnakpariyar@gmail.com>
2026-09-21 12:12:39 +0545

    ADD MAIN COMPONENT
    declare initial main component and styles

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+export default function Main() {
+    return (<main>
+        <form className="add-ingredient-form">
+            <input
+                type="Text"
+                placeholder="e.g. Oregano"
+                aria-label="Add Ingredient" />
+            <button>Add Ingredient</button>
+        </form>
+    </main>)
+}
