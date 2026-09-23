ff7195178abc68058e753238ef9060602ba64f9f
Rarnak <rarnakpariyar@gmail.com>
2026-09-23 16:07:10 +0545

    INTEGRATE AP API CALL FUNCTIONS
    integrate hugging face api call function, display ai response in the recipe component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,44 +1,21 @@
 import React from "react"
 import ClaudeRecipe from "./ClaudeRecipe"
 import IngredientsList from "./IngredientsList"
+import { getRecipeFromLlama } from "../../ai.js"
 export default function Main() {
 
-    /**
- * Challenge: clean up our code!
- * Let's make a couple new components to make things a
- * little cleaner. (Notice: I'm not suggesting what we
- * have now is bad or wrong. I'm mostly finding an excuse
- * to get in some hands-on practice 🙂)
- * 
- * 1. Move the entire recipe <section> into its own
- *    ClaudeRecipe component
- * 2. Move the list of ingredients <section> into its
- *    own IngredientsList component.
- * 
- * While you're considering how to structure things, consider
- * where state is, think about if it makes sense or not to
- * move it somewhere else, how you'll communicate between
- * the parent/child components, etc.
- * 
- * The app should function as it currently does when you're
- * done, so there will likely be some extra work to be done
- * beyond what I've listed above.
- */
-
     const [ingredients, setIngredients] = React.useState(["tomato", "oregano", "cheese", "milk"])
-    const [recipeShown, setRecipeShown] = React.useState(false)
-
-    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
+    const [recipe, setRecipe] = React.useState("")
 
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => ([...prevIngredients, newIngredient]))
     }
 
-    function toggleRecipeShown() {
-        setRecipeShown(prevRecipeShown => !prevRecipeShown)
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromLlama(ingredients)
+        console.log(recipeMarkdown)
+        setRecipe(recipeMarkdown)
     }
 
     return (
@@ -54,12 +31,10 @@ export default function Main() {
             </form>
             {ingredients.length > 0 ? <IngredientsList
                 ingredients = {ingredients}
-                ingredientsListItems = {ingredientsListItems}
-                toggleRecipeShown={toggleRecipeShown}
+                getRecipe={getRecipe}
             /> : null}
-
-
-            {recipeShown === true ? <ClaudeRecipe /> : null}
+            
+            {recipe? <ClaudeRecipe recipe={recipe}/> : null}
         </main>
     )
 }
